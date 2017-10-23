a79bc0944df147c33b7ec679e5a084125cace348
gaofeng <gaofeng3695@126.com>
2017-10-23 09:53:28 +0800

    修改了gitignore

---
+++ dist/src/js/pipeline_map/example.js
@@ -0,0 +1,1 @@
+var pipe_net_example={template:"#pipe_net_example",methods:{clickit:function(){alert("i click it")}},created:function(){console.log("pipe_net_example created")},mounted:function(){console.log("pipe_net_example mounted")}};
