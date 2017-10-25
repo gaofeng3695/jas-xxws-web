6426e644145931fccdc1cf353dcdfab38a79d672
gaofeng <gaofeng3695@126.com>
2017-10-25 19:00:00 +0800

    添加代理功能,修改gulp功能

---
+++ bs-config.js
@@ -0,0 +1,18 @@
+var  proxy  = require ('http-proxy-middleware');
+
+var  apiProxy = proxy('/c', {
+    target: 'http://192.168.100.213:8050/',
+    changeOrigin: true,
+    ws: true
+});
+
+
+
+module.exports = {
+
+    "server": {
+        middleware: [
+            apiProxy,
+        ]
+    }
+}
