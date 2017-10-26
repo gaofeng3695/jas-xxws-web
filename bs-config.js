efb4d712922ccb7e99dc3a9256cfb1ae959be010
gaofeng <gaofeng3695@126.com>
2017-10-26 17:11:06 +0800

    更新代理环境212

--- bs-config.js
+++ bs-config.js
@@ -1,12 +1,15 @@
 var  proxy  = require ('http-proxy-middleware');
 
+var argv = process.argv[2];
+
+var target = argv == '212'?'http://192.168.100.212:8050/':'http://192.168.100.213:8050/';
+
+
 var  apiProxy = proxy('/c', {
-    target: 'http://192.168.100.213:8050/',
+    target: target,
     changeOrigin: true,
     ws: true
 });
-
-
 
 module.exports = {
 
