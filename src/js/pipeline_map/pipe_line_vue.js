1b30ea1450698843d361a7df02110bad46f4392c
daixuan <daixuan@jasgroup.cn>
2017-11-02 17:43:19 +0800

    管线样式修改

--- src/js/pipeline_map/pipe_line_vue.js
+++ src/js/pipeline_map/pipe_line_vue.js
@@ -80,7 +80,7 @@ var pipe_line_list = {
             var styleobj = {
                 title: '新增管线',
                 width: '800',
-                height: '600',
+                height: '610',
             };
             var inputobj = {
                 "pipeLineName": "",
@@ -168,7 +168,7 @@ var pipe_net_list = {
             var styleobj = {
                 title: '新增管网',
                 width: '600',
-                height: '350',
+                height: '356',
             };
             var inputobj = {
                 "pipeNetworkName": "",
